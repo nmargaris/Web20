225799a77787cf1042bbd7d3e52ac2bd22754fb6
Nikos Margaris <58854775+nmargaris@users.noreply.github.com>
2022-12-26 17:34:47 +0200

    Update delete_db.php

--- demo1/delete_db.php
+++ demo1/delete_db.php
@@ -11,7 +11,7 @@ $result2=mysqli_query($conn, $sql2);
 if($result1 && $result2)
 {
     echo "Data successfully deleted";
-    //prepei afou diagrafontai ola ta arxeia twn xristwn gia kathe xristi to lastupload sthn basi na ginetai "0000-00-00 00:00:00"
+    
     $sql3="UPDATE users SET lastupload='0000-00-00 00:00:00'";
     $result3=mysqli_query($conn, $sql3);
 }
