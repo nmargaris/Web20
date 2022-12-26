446b09c8602da495f254ef3be29514ba665f5a5c
Nikos Margaris <58854775+nmargaris@users.noreply.github.com>
2022-12-26 17:45:44 +0200

    Update mapadmin.js

--- demo1/mapadmin.js
+++ demo1/mapadmin.js
@@ -10,21 +10,7 @@ mymap.setView([38.246242, 21.7350847], 12);
 $("#switch").click(function () {
     mymap.invalidateSize();
 });
-// $(document).ready(function () {
-//     $("#heatmapadmin").submit(function (e) {
-//         // let myArray = [];
-//         e.preventDefault();
-//         $.ajax({
-//             type: "POST",
-//             url: "getlocationsadmin.php",
-//             data: $("#heatmapadmin").serialize(),
-//             success: function (data) {
-//                 drawheatmap(data[0]);
-//             }
-//
-//         })
-//     })
-// })
+
 
 function submitform() {
     var data = $("#heatmapadmin").serialize();
@@ -45,16 +31,6 @@ function submitform() {
 
 function submitexport(){
     const data = $("#heatmapadmin").serialize();
-    // $.ajax({
-    //     type: 'POST',
-    //     url: 'export.php',
-    //     data: data,
-    //     success: function (data) {
-    //         console.log(data);
-    //         confirm("Your data has been exported");
-    //
-    //     }
-    // });
     $.ajax({
         type: "POST",
         url: 'export.php',
@@ -100,50 +76,7 @@ function submitexport(){
         }
     });
 }
-// $.ajax({
-//     type: "POST",
-//     url: url,
-//     data: params,
-//     success: function(response, status, xhr) {
-//         // check for a filename
-//         var filename = "";
-//         var disposition = xhr.getResponseHeader('Content-Disposition');
-//         if (disposition && disposition.indexOf('attachment') !== -1) {
-//             var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
-//             var matches = filenameRegex.exec(disposition);
-//             if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
-//         }
-//
-//         var type = xhr.getResponseHeader('Content-Type');
-//         var blob = new Blob([response], { type: type });
-//
-//         if (typeof window.navigator.msSaveBlob !== 'undefined') {
-//             // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
-//             window.navigator.msSaveBlob(blob, filename);
-//         } else {
-//             var URL = window.URL || window.webkitURL;
-//             var downloadUrl = URL.createObjectURL(blob);
-//
-//             if (filename) {
-//                 // use HTML5 a[download] attribute to specify filename
-//                 var a = document.createElement("a");
-//                 // safari doesn't support this yet
-//                 if (typeof a.download === 'undefined') {
-//                     window.location = downloadUrl;
-//                 } else {
-//                     a.href = downloadUrl;
-//                     a.download = filename;
-//                     document.body.appendChild(a);
-//                     a.click();
-//                 }
-//             } else {
-//                 window.location = downloadUrl;
-//             }
-//
-//             setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
-//         }
-//     }
-// });
+
 
 function drawheatmap(data) {
     let layercount = 0;
