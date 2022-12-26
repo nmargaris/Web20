0a96ce11928dbb45791bb9dd016a51f6077f9682
Nikos Margaris <58854775+nmargaris@users.noreply.github.com>
2022-12-26 17:44:59 +0200

    Update map.js

--- demo1/map.js
+++ demo1/map.js
@@ -3,7 +3,6 @@ let osmUrl='https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 let osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
 let osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
 mymap.addLayer(osm);
-//sintetagmenes kai zoom
 mymap.setView([38.246242, 21.7350847], 12);
 $(document).ready(function () {
     $("#heatmap").submit(function (e) {
